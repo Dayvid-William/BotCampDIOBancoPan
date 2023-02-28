844dd3041d47487d5f9fc1b22f0ca7ebae68f458
Dayvid-William <dayvid.william5@outlook.com>
2023-02-28 15:36:28 -0300

    operador ternario

--- Java/SintaxeJAVA/java-anatomia-classes/src/edu/dayvid/operadores/operadores.java
+++ Java/SintaxeJAVA/java-anatomia-classes/src/edu/dayvid/operadores/operadores.java
@@ -19,5 +19,32 @@ public class operadores {
     concatenacao = "1"+(1+1+1);
 
     System.out.println(concatenacao);
+
+    int num = 5;
+
+    //operador de incremento
+    num = ++num;
+
+    System.out.println(num);
+    System.out.println(++num);
+
+    //inverte variavel boolen
+
+    boolean verdade = true;
+
+    verdade = !verdade;
+
+    System.out.println(verdade);
+
+    //operador ternario
+
+    int a, b;
+
+    a = 5;
+    b = 5;
+
+    String resultador = a == b ? "Verdadeiro" : "Falso";
+
+    System.out.println(resultador);
   }
 }
