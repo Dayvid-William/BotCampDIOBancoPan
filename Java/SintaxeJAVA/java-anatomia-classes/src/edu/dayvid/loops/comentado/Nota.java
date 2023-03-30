735a3dad1d5652484ee1a5ad2d91a70bffaddeb9
Dayvid-William <dayvid.william5@outlook.com>
2023-03-30 11:36:44 -0300

    exercicio nota

---
+++ Java/SintaxeJAVA/java-anatomia-classes/src/edu/dayvid/loops/comentado/Nota.java
@@ -0,0 +1,23 @@
+package edu.dayvid.loops.comentado;
+
+import java.util.Scanner;
+
+/*
+ * Faça um programa que peça uma notra, entre zero e dez.
+ * mostre uma mensagem caso o valor seja invalido e continue pedindo
+ * até que o usuário infomre um valor valido
+ */
+
+public class Nota {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int nota;
+
+    while (true) {
+      System.out.println("nota: ");
+      nota = scan.nextInt();
+      if (nota >= 0 && nota <= 10) break ;
+    }
+  }
+}
